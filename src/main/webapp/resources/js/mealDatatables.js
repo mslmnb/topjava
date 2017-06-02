9800f16edf880960b71c630a600fc54bf8317686
“mslmnb” <mslmn@rambler.ru>
2017-06-02 22:05:48 +0600

    Занятие 10 Видео1 HW09 Binding, Validation, Ajax для Meal. Вынести формирование массива i18n[] в отдельный i18n.jsp. Формирование  строки сообщений об ошибках выделим в отдельный метод

--- src/main/webapp/resources/js/mealDatatables.js
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -18,11 +18,21 @@ function clearFilter() {
 
 $(function () {
     datatableApi = $("#datatable").DataTable({
+        "ajax": {
+            "url": ajaxUrl,
+            "dataSrc": ""
+        },
         "paging": false,
         "info": true,
         "columns": [
             {
-                "data": "dateTime"
+                "data": "dateTime",
+                "render": function (date, type, row) {
+                    if (type === 'display') {
+                        return date.replace('T', ' ').substr(0, 16);
+                    }
+                    return date;
+                }
             },
             {
                 "data": "description"
@@ -31,11 +41,13 @@ $(function () {
                 "data": "calories"
             },
             {
-                "defaultContent": "Edit",
+                "render": renderEditBtn,
+                "defaultContent": "",
                 "orderable": false
             },
             {
-                "defaultContent": "Delete",
+                "render": renderDeleteBtn,
+                "defaultContent": "",
                 "orderable": false
             }
         ],
@@ -44,7 +56,10 @@ $(function () {
                 0,
                 "desc"
             ]
-        ]
+        ],
+        "createdRow": function (row, data, dataIndex) {
+            $(row).addClass(data.exceed ? 'exceeded' : 'normal');
+        },
+        "initComplete": makeEditable
     });
-    makeEditable();
 });
