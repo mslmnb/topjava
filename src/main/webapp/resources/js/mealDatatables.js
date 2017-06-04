0c7706fc01cca874a5ff4db850cef76e80563f15
“mslmnb” <mslmn@rambler.ru>
2017-06-04 21:11:24 +0600

    Занятие 10 Видео2 Кастомизация контроллера и моделей для еды. Тест

--- src/main/webapp/resources/js/mealDatatables.js
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -26,13 +26,7 @@ $(function () {
         "info": true,
         "columns": [
             {
-                "data": "dateTime",
-                "render": function (date, type, row) {
-                    if (type === 'display') {
-                        return formatDate(date);
-                    }
-                    return date;
-                }
+                "data": "dateTimeUI"
             },
             {
                 "data": "description"
